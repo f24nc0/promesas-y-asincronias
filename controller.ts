8d894be7f6ff12d4694dc44daf28d3bce77887e6
unknown <francolepanto@gmail.com>
2023-12-07 20:44:13 -0300

    primer uso de async/await

--- controller.ts
+++ controller.ts
@@ -12,14 +12,21 @@ class ProductController{
     const coll = new ProductCollection
     this.products = coll
   }
-  processOptions(option:ProductControllerOptions){
+  async processOptions(option:ProductControllerOptions){
     if (option.action == "search"&& typeof option.params == "number"){
-      return this.products.getById(option.params).then((res)=>{
+  /*     return this.products.getById(option.params).then((res)=>{
         return res
-      })
-    } else return this.products.getAll().then((res)=>{
-      return res
-    })
+      })*/
+      const respuesta = await this.products.getById(option.params)
+      return respuesta
+
+    } else {
+  /*    return this.products.getAll().then((res)=>{
+        return res
+      })*/
+      const respuesta = await this.products.getAll()
+      return respuesta 
+    }
   }
 }
 
--- models.ts
+++ models.ts
@@ -8,19 +8,22 @@ class Product{
 class ProductCollection{
   products:Product[] = []
 
-  getAll(){
-    return jsonfile.readFile("./products.json").then((productos)=>{
+  async getAll(){
+    /*return jsonfile.readFile("./products.json").then((productos)=>{
       return productos
-    })
+    })*/
+
+    const res = await jsonfile.readFile("./products.json")
+    this.products = res
+    return res
   }
 
-  getById(id:number){
-    return this.getAll().then((productos)=>{
-      const resultado = productos.find((producto)=>{
-        return producto.id == id
-      })
-      return resultado
+  async getById(id:number){
+    const productos = await this.getAll()
+    const resultado = productos.find((producto)=>{
+      return producto.id == id
     })
+    return resultado
   }
 }
 
